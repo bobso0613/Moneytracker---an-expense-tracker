fd041b09bbc367bb6a6734538ac627dde79c1f56
Bob <bobdaniel99@yahoo.com>
2022-09-05 12:44:00 +0800

    change

--- db/api/DatabaseConnect.php
+++ db/api/DatabaseConnect.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 
 //ini_set('error_reporting', E_ALL|E_STRICT);
-//ini_set('display_errors', 1);
+//ini_set('display_errors', 1);asdsad
 	//echo 'ea ea';
 	include_once("DatabaseParams.php");
 	include_once("mysql-php7-shim.php");
